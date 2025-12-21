6decc24d8994e2c75b99cfbe4d1eacfe6f79cd7e
Alex Marian <alexandrumarian19@gmail.com>
2025-12-22 00:24:55 +0200

    - added basic object types  - added evaluation

---
+++ evaluator/evaluator.go
@@ -0,0 +1,70 @@
+package evaluator
+
+import (
+	"github.com/alexmarian/interpretor/ast"
+	"github.com/alexmarian/interpretor/object"
+)
+
+var (
+	TRUE = &object.Boolean{
+		Value: true,
+	}
+	FALSE = &object.Boolean{
+		Value: false,
+	}
+	NULL = &object.Null{}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	}
+	return nil
+}
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+
+	}
+}
+func nativeBoolToBooleanObject(value bool) *object.Boolean {
+	if value {
+		return TRUE
+	} else {
+		return FALSE
+	}
+}
